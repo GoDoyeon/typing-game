103e713b4253d730aa2d3f95bc30fb505b7c996f
YOSEPHLEE <laksmi0628@gmail.com>
2021-06-01 15:34:34 +0900

    feat : CSS 및 JS 기능 추가
    main.css에 백그라운드 색깔을 변경했습니다.
    START버튼을 클릭시 게임이 시작되고 setTimeout을 사용하여 시간 변화를 주었습니다.

---
+++ main.js
@@ -0,0 +1,46 @@
+
+
+const startBtn = document.querySelector('.startBtn');
+
+const gameContainer = document.querySelector('.gameContainer'),
+    clockTitle = gameContainer.querySelector('h2');
+
+// startBtn 클릭이벤트
+
+startBtn.addEventListener('click', startGame);
+
+function startGame() {
+    gameContainer.style.display = 'block';
+    startBtn.style.display = 'none';
+    startClock();
+}
+
+// 시간 설정
+
+let seconds = 0,
+    minutes = 0,
+    hours = 0;
+
+
+function startClock() {
+    add();
+    
+}
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+    timer();
+}
+function timer() {
+    setTimeout(add, 1000);
+}
