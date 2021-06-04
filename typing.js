66a0b501503079b85ea05d46184798e36677956f
YOSEPHLEE <laksmi0628@gmail.com>
2021-06-04 09:45:59 +0900

    feat : 점수 기능 생성 및 랜덤단어생성 수정
    단어를 맞추면 1~4점을 랜덤으로 배점하는 기능 생성했습니다.
    랜덤 단어 생성을 중복된 단어가 나오지 않도록 했습니다.

--- typing.js
+++ typing.js
@@ -4,9 +4,12 @@ const wordContainer = document.querySelector('.wordContainer'),
 const inputForm = document.querySelector('.inputForm'),
     inputText = inputForm.querySelector('input');
 
-const arr = [
+const scoreContainer = document.querySelector('.scoreContainer'),
+    scoreText = scoreContainer.querySelector('span');
+
+const wordArray = [
     'apple',
-    'banna',
+    'banana',
     'cup',
     'mouse',
     'computer',
@@ -21,34 +24,70 @@ const arr = [
     'line',
     'flower',
     'door',
-    'window'
+    'window',
+    'star',
+    'mask',
+
 ];
 
-function randomItem(item) {
-    return item[Math.floor(Math.random() * item.length)];
+
+
+
+//랜덤 단어
+function randomWord() {
+    
+    let newWordArray = [];
+    
+    let randomArr = wordArray.splice(Math.floor(Math.random() * wordArray.length), 1)[0];
+    newWordArray.push(randomArr);
+    let randomWord = newWordArray[0];
+    word.innerText = randomWord;
 
 }
-word.innerText = randomItem(arr);
+
  
+// 틀린단어
+function validation(e) {
+    e = inputText.value;
+    let currentValue = e;
+    let innerTextWord = word.innerText;
 
-inputForm.addEventListener("submit", handleSubmit);
-
-function handleSubmit(e) {
-    e.preventDefault();
-    const currentValue = inputText.value;
-    paintText(currentValue);
-    inputText.value = "";
-}
-
-
-function paintText(text) {
-    if (text === word.innerText) {
-        word.innerText = randomItem(arr);
-    } else {
-       word.innerText = randomItem(arr);
+    if (currentValue != innerTextWord) {
+        inputText.classList.add('outLine');
+       //여기서 3번 돌아가면 alert창 뜨고 종료
     }
+    
+    if (currentValue === '' || currentValue === innerTextWord) {
+        inputText.classList.remove('outLine');
+        inputForm.addEventListener("submit", answerSubmit);
+    }
+
+}
+
+
+// 점수 추가
+function addScore() {
+
+    let score = 0;
+    const randomScore = [1, 2, 3, 4];
+
+    score = randomScore[Math.floor(Math.random() * randomScore.length)];
+    scoreText.innerText = parseInt(scoreText.innerText) + score;
 }
 
 
 
+//정답일때
+function answerSubmit(e) {
+    e.preventDefault();
+    randomWord();
+    inputText.value = "";
+    addScore();
+}
 
+
+function init() {
+    randomWord();
+    inputText.addEventListener("input", validation);
+}
+init();
